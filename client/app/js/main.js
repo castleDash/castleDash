6e58b921598c4ca1fcc4e4d65cbae1e7daff98ac
Sally Kingston <sally.a.kingston@gmail.com>
2015-12-13 13:56:38 -0500

    made login mage show game

--- client/app/js/main.js
+++ client/app/js/main.js
@@ -1,30 +1,44 @@
 var login = {
   init: function(){
-    login.styling();
     login.events();
-
   },
-  styling: function(){
 
-  },
   events: function(){
     $("#loginBtn").on("click", function () {
       var username = $("input[type='username']");
       var password = $("input[type='password']");
       $("input[type='username']").val("");
       $("input[type='password']").val("");
-      // $.ajax({
-      //   method: "POST",
-      //   url: "/login",
-      //   data: { username: username, password: password }
-      // })
-      //   .success(function( data ) {
-      //     //check for successful login
-      //     if (data){
+      $.ajax({
+        method: "POST",
+        url: "/login",
+        data: { username: username, password: password}
+      })
+        .success(function( data ) {
+          //check for successful login
+          if (data){
             $("#login").addClass("hidden");
             $("#game").removeClass("hidden");
-    //       };
-    //     });
+          };
+        });
+    })
+    $("#registerBtn").on("click", function () {
+      var username = $("input[type='username']");
+      var password = $("input[type='password']");
+      $("input[type='username']").val("");
+      $("input[type='password']").val("");
+      $.ajax({
+        method: "POST",
+        url: "/createUser",
+        data: { username: username, password: password}
+      })
+        .success(function( data ) {
+          //check for successful login
+          if (data){
+            $("#login").addClass("hidden");
+            $("#game").removeClass("hidden");
+          };
+        });
     })
   }
 
