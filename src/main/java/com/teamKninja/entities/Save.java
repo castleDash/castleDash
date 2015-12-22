7e2a006c1d9c65bebce77f549f8ab0e3e89ddc23
holdenhughes <holdenmh121489@gmail.com>
2015-12-22 13:21:41 -0500

    Removed save variables

--- src/main/java/com/teamKninja/entities/Save.java
+++ src/main/java/com/teamKninja/entities/Save.java
@@ -18,18 +18,6 @@ public class Save {
     @Column(nullable = false)
     public int score;
 
-    @Column(nullable = false)
-    public int healthPotion;
-
-    @Column(nullable = false)
-    public int firePotion;
-
-    @Column(nullable = false)
-    public int shieldPotion;
-
-    @Column(nullable = false)
-    public int health;
-
     @ManyToOne
     public User user;
 
