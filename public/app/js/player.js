6ec4ed729df9fec07e5776f60ec03d571d64fee9
DrScott <dougscott2@gmail.com>
2016-01-05 13:40:27 -0500

    rewored level completion parameters

--- public/app/js/player.js
+++ public/app/js/player.js
@@ -41,7 +41,8 @@ castlePlayer.prototype = {
     },
 
     update: function() {
-        if (player.body.x>=castleStage.endTile[0].x && player.body.y>=castleStage.endTile[0].y && player.body.y<=(castleStage.endTile[0].y+32)){
+        if (player.body.x>=castleStage.endTile[0].x && player.body.y>=castleStage.endTile[0].y &&
+           player.body.x <=(castleStage.endTile[0].x+5) && player.body.y<=(castleStage.endTile[0].y+32)){
             if(castleStage.levelName !="tutorial"){
               this.currentLevel = this.currentLevel +1;
               this.previousGold = this.gold;
